959bf3b6a7247c18b7ac001d78aba929488c6938
RafalB <rafikimielec@gmail.com>
2020-02-09 13:13:34 +0100

    If applied, this commit will update queries

---
+++ Project1/sql_queries.py
@@ -0,0 +1,158 @@
+# CREAT ENUM TYPES
+enum_subscriber = """CREATE TYPE subscriber AS ENUM ('free', 'paid', 'NaN')"""
+enum_gender = """CREATE TYPE gender AS ENUM ('F', 'M')"""
+
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
+# CREATE TABLES
+
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+                        user_id int NOT NULL PRIMARY KEY,
+                        first_name varchar,
+                        last_name varchar,
+                        gender gender,
+                        level subscriber
+                      );
+""")
+
+song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+                        song_id varchar PRIMARY KEY,
+                        tittle varchar,
+                        artist_id varchar NOT NULL, 
+                        year int,
+                        duration real
+                      );
+""")
+
+artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+                        artist_id varchar PRIMARY KEY,
+                        name varchar,
+                        location varchar,
+                        latitude real,
+                        longitude real
+                       );
+""")
+
+time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+                        start_time timestamp NOT NULL,
+                        hour int,
+                        day int,
+                        week int,
+                        month int,
+                        year int,
+                        weekday int
+                       );
+""")
+
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+                        songplay_id int NOT NULL PRIMARY KEY,
+                        start_time timestamp NOT NULL,
+                        user_id int NOT NULL,
+                        level subscriber,
+                        song_id varchar,
+                        artist_id varchar,
+                        session_id int NOT NULL,
+                        location varchar,
+                        user_agent varchar,
+                        FOREIGN KEY (user_id) REFERENCES users(user_id),
+                        FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
+                        FOREIGN KEY (song_id) REFERENCES songs(song_id)
+                       );
+""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+INSERT INTO songplays (
+            songplay_id, 
+            start_time, 
+            user_id, 
+            level, 
+            song_id, 
+            artist_id,
+            session_id,
+            location,
+            user_agent
+           )
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (songplay_id)
+DO NOTHING;
+""")
+
+user_table_insert = ("""
+INSERT INTO users (
+            user_id, 
+            first_name, 
+            last_name, 
+            gender,
+            level
+           )
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id) 
+DO UPDATE SET level = EXCLUDED.level;
+""")
+
+song_table_insert = ("""
+INSERT INTO songs (
+            song_id, 
+            tittle,
+            artist_id,
+            year,
+            duration)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (song_id)
+DO NOTHING;
+""")
+
+artist_table_insert = ("""
+INSERT INTO artists (
+            artist_id,
+            name,
+            location,
+            latitude,
+            longitude) 
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id)
+DO UPDATE SET location = EXCLUDED.location,
+              latitude = EXCLUDED.latitude,
+              longitude = EXCLUDED.longitude;
+""")
+
+
+time_table_insert = ("""
+INSERT INTO time (
+            start_time,
+            hour,
+            day,
+            week,
+            month,
+            year,
+            weekday)
+VALUES (%s, %s, %s, %s, %s, %s, %s);
+""")
+
+# FIND SONGS
+
+song_select = ("""
+SELECT * FROM songs
+JOIN artists ON songs.artist_id=artists.artist_id
+WHERE songs.tittle = %s 
+AND artists.name = %s 
+AND songs.duration = %s;
+""")
+
+# QUERY LISTS
+enum_queries = [enum_gender, enum_subscriber]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
